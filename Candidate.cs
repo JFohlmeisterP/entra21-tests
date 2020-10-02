66f20de931115fe7bee4d98f7424043dc11167ea
Entra21 <entra21@proway.treina>
2020-10-01 21:08:15 -0300

    Classe Candidate criada

---
+++ Candidate.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace entra21_tests
+{
+    public class Candidate
+    {
+        private Guid id {get; set;}
+        private string name {get; set;}
+        private string cpf {get; set;}
+        private int vote {get; set;}
+
+    }
+
+    
+
+}
